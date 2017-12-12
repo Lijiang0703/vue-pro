ddfe6f5bd92537ccd185c0a7f8280c329e1a3d6e
lijiang0703 <li_jiang0703@outlook.com>
2017-12-12 23:54:02 +0800

    state

---
+++ ll-music/src/common/js/config.js
@@ -0,0 +1,6 @@
+export mode ={
+	sequence:0,
+	random:1,
+	loop:2
+}
+export {mode}
--- ll-music/src/store/state.js
+++ ll-music/src/store/state.js
@@ -1,4 +1,11 @@
+import {mode} from 'common/js/config'
+
 const state = {
-	singer:{}
+	singer:{},
+	songlist:[],  //播放列表
+	fullScreen:false, //是否全屏
+	mode: mode.sequence, 	// 模式
+	play:false,
+	currentIndex:-1
 }
 export default state
